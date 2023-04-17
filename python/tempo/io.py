34453c3aea6373a7db070f76f5a2c02914abad90
Sonali Guleria <sonali.guleria@outlook.com>
2023-04-17 16:01:20 -0400

    io changes

--- python/tempo/io.py
+++ python/tempo/io.py
@@ -16,19 +16,21 @@ def write(
     spark: SparkSession,
     table_name: str,
     optimization_cols: list[str] = None,
-    mode="append",
-    options={},
-    trigger_options={},
+    mode: str = "append",
+    options: dict = {},
+    trigger_options: dict = {},
 ):
     """
     param: tsdf: input TSDF object to write
-    param: table_name Delta output table name
-    param: optimization_cols list of columns to optimize on (time)
+    param: table_name: Delta output table name
+    param: optimization_cols: list of columns to optimize on (time)
+    param: mode: write mode for delta. default: "Append"
+    param: options: Additional options for delta writes
+    param: trigger_options: The trigger settings of a streaming query define the timing of streaming data processing
     """
     # hilbert curves more evenly distribute performance for querying multiple columns for Delta tables
     spark.conf.set("spark.databricks.io.skipping.mdc.curve", "hilbert")
 
-    tsdf.df
     ts_col = tsdf.ts_col
     partitionCols = tsdf.partitionCols
     view_df = tsdf.df.withColumn("event_dt", f.to_date(f.col(ts_col))).withColumn(
@@ -37,39 +39,32 @@ def write(
             "double"
         ),
     )
+    view_cols = deque(view_df.columns)
+    view_cols.rotate(2)
+    view_df = view_df.select(*list(view_cols))
 
     if not view_df.isStreaming:
         if optimization_cols:
             optimization_cols = optimization_cols + ["event_time"]
         else:
             optimization_cols = ["event_time"]
-
-        view_cols = deque(view_df.columns)
-        view_cols.rotate(1)
-        view_df = view_df.select(*list(view_cols))
-
-        view_cols = deque(view_df.columns)
-        view_cols.rotate(1)
-        view_df = view_df.select(*list(view_cols))
-
         view_df.write.mode(mode).partitionBy("event_dt").options(**options).format(
             "delta"
         ).saveAsTable(table_name)
 
         try:
             spark.sql(
-                "optimize {} zorder by {}".format(
-                    table_name, "(" + ",".join(partitionCols + optimization_cols) + ")"
-                )
+                f"optimize {table_name} zorder by {'(' + ','.join(partitionCols + optimization_cols) + ')'}"
             )
         except ParseException as e:
             logger.error(
-                "Delta optimizations attempted, but was not successful.\nError: {}".format(
-                    e
-                )
+                f"Delta optimizations attempted, but was not successful.\nError: {e}"
             )
     else:
         if "checkpointLocation" not in options:
+            logger.warning(
+                f"No Checkpoint location provided in options; using default /tmp/tempo/streaming_checkpoints/{table_name}"
+            )
             options["checkpointLocation"] = (
                 "/tmp/tempo/streaming_checkpoints/" + table_name
             )
